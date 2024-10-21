7c5ff4e2c5944dfa6e587ed6bfe69fdcc1c41865
SHSW-Syu <841418419z@gmail.com>
2024-10-21 23:48:23 +0900

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom'; // 修改为 react-dom
 import { BrowserRouter as Router } from 'react-router-dom'; // 导入 Router
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root') // 将渲染目标作为第二个参数传入
 );
 
 // 如果您想在应用中开始测量性能，可以传递一个函数来记录结果
